620eca9c1df1c7172c1c56f25fe7411c49a352fb
Delaney Green <delaney.rosa1@gmail.com>
2024-04-24 10:22:25 -0500

    Page now displays randomized groups and has clear button to reset the form and clear the groups

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 window.addEventListener("load", function(){
-    const form = this.document.getElementById("namesForm")
+    const form = document.getElementById("namesForm")
+    const groupDisplay = document.getElementById("groupDisplay");
+    const clearButton = document.getElementById("clear");
 
     form.addEventListener("submit", function(event){
         event.preventDefault();
         const namesInput = document.getElementById('namesInput').value; 
-        const numStudentInput = parseInt(document.getElementById("numInput").value);
+        const numRooms = parseInt(document.getElementById("numInput").value);
 
         let namesArray = namesInput.split(",");
         namesArray = namesArray.map(function(name){
             return name.trim();
         });
 
-        console.log(namesArray);
-        console.log(numStudentInput);
+        let groups = randomize(namesArray,numRooms);
+        let htmlContent = '';
+        groups.forEach(function(group, index){
+            htmlContent += `<div class="group">
+                                <h3>Group ${index + 1}</h3>
+                                <ul>`;
+            group.forEach(function(student){
+                htmlContent += `<li>${student}</li>`;
+            });
+            htmlContent += `</ul>
+                           </div>`;
+        });
+        groupDisplay.innerHTML = htmlContent;
     });
 
-    function randomize(nameArray,numStudents){
-        
+    clearButton.addEventListener("click", function(){
+        let userConfirm = window.confirm("are you sure you want to clear your class info?")
+        if (userConfirm){
+            form.reset();
+            groupDisplay.innerHTML = "waiting for input"
+        }
+    });
+
+
+    function randomize(nameArray,numRooms){
+        let studentsPerRoom = Math.ceil(nameArray.length / numRooms);
+        let randomArrays = [];
+
+        for (let i =0; i<numRooms; i++){
+            randomArrays.push([]);
+        }
+
+        while(nameArray.length>0){
+            for (let i=0; i<randomArrays.length && nameArray.length; i++){
+                let randIdx = Math.floor(Math.random()*nameArray.length);
+                let student = nameArray.splice(randIdx,1)[0];
+                randomArrays[i].push(student);
+            }
+        }
+        return randomArrays;
     }
 
-})
+
+
+
+});
